fe0dfd91b4b3d4559b96a4f5902d616e1fbf1e00
timczepizak <tim_czepizak@hotmail.com>
2019-09-13 17:14:23 -0500

    Alpha Commit

---
+++ Project Number 1 CS 428/Assets/ManScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ManScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject placement;
+    public GameObject Man;
+    public GameObject Button;
+    public Animator manAnim;
+    private bool flag = false;
+    private Transform t;
+    public new AudioSource audio;
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = placement.transform;
+        Button = GameObject.Find("NoahButton");
+        Button.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        manAnim.GetComponent<Animator>();
+    }
+
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        if (!flag)
+        {
+            flag = true;
+            Instantiate(Man,t);
+            manAnim.Play("Man");
+            audio.Play();
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        manAnim.Play("none");
+        
+    }
+
+   
+}
